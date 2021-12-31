2af2bca6b8b6d83db3629093c9f8cc705f44a9c9
Brendon Martin <brendonmartin657@gmail.com>
2021-12-31 11:51:11 -0500

    Added HTML & JS to display digital clock & the date.

--- assests/js/script.js
+++ assests/js/script.js
@@ -1,9 +1,15 @@
 const hourHand = document.getElementById("clock-hour"),
   minuteHand = document.getElementById("clock-minutes"),
-  secondHand = document.getElementById("clock-seconds");
+  secondHand = document.getElementById("clock-seconds"),
+  digitalHours = document.getElementById("text-hours"),
+  digitalMinutes = document.getElementById("text-minutes"),
+  digitalSeconds = document.getElementById("text-seconds"),
+  day = document.getElementById("date-day"),
+  month = document.getElementById("date-month"),
+  year = document.getElementById("date-year");
 
 const clock = () => {
-  let date = new Date();
+  const date = new Date();
 
   // We need to sync up each hour, minute, and second with the degrees in a circle
   // There's 360deg in a circle. So, we'll divide 360deg by the total possible hours, minutes, and seconds.
@@ -11,7 +17,7 @@ const clock = () => {
   // 360 / 60 = 6  | Minutes & Seconds
   // Every hour we rotate the hour hand 30deg and every minute / second we rotate those hands 6deg.
 
-  let hourHandDegrees = date.getHours() * 30,
+  const hourHandDegrees = date.getHours() * 30,
     minuteHandDegrees = date.getMinutes() * 6,
     secondHandDegrees = date.getSeconds() * 6;
 
@@ -21,4 +27,22 @@ const clock = () => {
   secondHand.style.transform = `rotateZ(${secondHandDegrees}deg)`;
 };
 
+const displayTime = () => {
+  const date = new Date();
+
+  const hours = date.getHours(),
+    minutes = date.getMinutes(),
+    seconds = date.getSeconds();
+
+  digitalHours.innerText = `${hours >= 10 ? hours + ":" : "0" + hours + ":"}`;
+  digitalMinutes.innerText = `${minutes >= 10 ? minutes + ":" : "0" + minutes + ":"}`;
+  digitalSeconds.innerText = `${seconds >= 10 ? seconds : "0" + seconds}`;
+
+  month.innerText = `${date.getMonth() + 1} /`;
+  day.innerText = `${date.getDate()} /`;
+  year.innerText = `${date.getFullYear()}`;
+};
+
+
 setInterval(clock, 1000);
+setInterval(displayTime, 1000);
